d0fe57ffac354a196e7be51e06dbed03dbd09b5c
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-07-26 13:57:23 -0400

    Prompt the user to  pick a number beween 1 and 100

--- guessing_game.rb
+++ guessing_game.rb
@@ -5,4 +5,10 @@ Game plan:
 have program select a random number and assign it to a variable named mac_picked
 mac_picked = 1 + rand(100)
 
+2 [ ] Prompt you for a guess
+Game plan:
+Use puts "Please pick a number beween 1 and 100" to prompt the user
+Use gets.chomp.to_i to collect the input and assign it to a variable named user_picked
+user_picked = gets.chomp.to_i
+
 =end
