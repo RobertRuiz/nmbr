4a0321bb9868c5ef22176c3870c54c300553edce
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-07-26 14:57:06 -0400

    It is a working

--- guessing_game.rb
+++ guessing_game.rb
@@ -47,7 +47,6 @@ end
 Game plan:
 Need to create a variable, name it count,
 count = 0
-
 have it increase after each guess
 count += 1
 
@@ -55,13 +54,35 @@ and another if then statement
 if count > 4
   puts "Your crystal ball is foggy, you lose"
 end
-
 This would likely need to go above the comparison loop ?
 
 [ ] Your code should use at least two methods. NOTE this doesn't say in a class --
 but if you feel like there is a good data+behavior approach, go for it.
 Game plan:
+Not sure what a method is yet? - 
 
 [ ] This game should be run from the command line by typing "ruby guessing_game.rb"
 
 =end
+
+# Let's code
+count = 0
+mac_picked = 1 + rand(100)
+
+loop do
+  puts "Please pick a number beween 1 and 100"
+  # (CHEAT MODE: the answer is #{mac_picked})"
+  user_picked = gets.chomp.to_i
+  count += 1
+  if count >= 5
+    puts "Your crystal ball is foggy, you lose"
+    break
+  elsif user_picked == mac_picked
+    puts "You are correct, great guess, book your trip to Vegas immediatedly"
+    break
+  elsif user_picked > mac_picked
+    puts "Your guess is high, please guess again"
+  elsif user_picked < mac_picked
+    puts "Your guess is low, please guess again"
+  end
+end
