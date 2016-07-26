8642f0e6513dc6432f63a93b594aff7a1d592235
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-07-26 13:54:57 -0400

    Pick a random number between 1 and 100

---
+++ guessing_game.rb
@@ -0,0 +1,8 @@
+=begin
+
+1 [ ] Pick a random number between 1 and 100
+Game plan:
+have program select a random number and assign it to a variable named mac_picked
+mac_picked = 1 + rand(100)
+
+=end
