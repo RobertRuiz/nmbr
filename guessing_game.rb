aa69cb926a9c9970771bdc1afa807d6e5e58e92c
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-07-26 13:58:27 -0400

    use if, else to compare user_picked vs. mac_picked

--- guessing_game.rb
+++ guessing_game.rb
@@ -11,4 +11,13 @@ Use puts "Please pick a number beween 1 and 100" to prompt the user
 Use gets.chomp.to_i to collect the input and assign it to a variable named user_picked
 user_picked = gets.chomp.to_i
 
+3 [ ] If your guess is less than the program's number,
+it should tell you that you were low and let you guess again.
+Game plan:
+use if, else to compare user_picked vs. mac_picked
+
+if user_picked < mac_picked
+  puts "Your guess is low, please guess again"
+end
+
 =end
